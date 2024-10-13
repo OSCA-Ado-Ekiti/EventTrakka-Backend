5725ad1c2b1b9486f3045f08493d325edf786080
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-10-13 18:32:59 +0100

    Fix BaseModelManager implementation

--- app/managers.py
+++ app/managers.py
@@ -5,45 +5,56 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlmodel import delete, select
 
 from app.core.db import get_db_session
+from app.core.utils import aware_datetime_now
 
 
 class BaseModelManager:
     def __init__(self, model_class):
         self.model_class: type[BaseModel] = model_class
-        self.session = get_db_session()
 
     async def create(
-        self, creation_data: dict, session: AsyncSession | None = None
+        self, *, creation_data: dict, session: AsyncSession | None = None
     ) -> BaseModel:
-        session = session or self.session
-        model = self.model_class.model_validate(creation_data)
-        session.add(model)
-        await session.commit()
-        await session.refresh(model)
-        return model
+        async for s in get_db_session():
+            session = s or session
+            if "last_updated_on" not in creation_data.keys():
+                creation_data["last_updated_on"] = aware_datetime_now()
+            model = self.model_class.model_validate(creation_data)
+            session.add(model)
+            await session.commit()
+            await session.refresh(model)
+            return model
 
     async def update(
-        self, id: UUID, update_data: dict, session: AsyncSession | None = None
+        self, *, id: UUID, update_data: dict, session: AsyncSession | None = None
     ):
-        session = session or self.session
-        model = await self.get(id=id, session=session)
-        model = self.model_class.model_validate(model.model_dump().update(update_data))
-        session.add(model)
-        await session.commit()
-        await session.refresh(model)
-        return model
+        async for s in get_db_session():
+            session = s or session
+            if "last_updated_on" not in update_data.keys():
+                update_data["last_updated_on"] = aware_datetime_now()
+            model = await self.get(id=id, session=session)
+            model = self.model_class.model_validate(
+                model.model_dump().update(update_data)
+            )
+            session.add(model)
+            await session.commit()
+            await session.refresh(model)
+            return model
 
-    async def get(self, id: UUID, session: AsyncSession | None = None) -> BaseModel:
-        session = session or self.session
-        query = select(self.model_class).where(self.model_class.id == id)
-        return (await session.execute(query)).scalar_one()
+    async def get(self, session: AsyncSession | None = None, *whereclause) -> BaseModel:
+        async for s in get_db_session():
+            session = s or session
+            query = select(self.model_class).where(*whereclause)
+            return (await session.execute(query)).scalar_one()
 
-    async def all(self, session: AsyncSession | None = None):
-        session = session or self.session
-        query = select(self.model_class)
-        return (await session.execute(query)).all()
+    async def all(self, *, session: AsyncSession | None = None):
+        async for s in get_db_session():
+            session = s or session
+            query = select(self.model_class)
+            return (await session.execute(query)).all()
 
-    async def delete(self, id: UUID, session: AsyncSession | None):
-        session = session or self.session
-        query = delete(self.model_class).where(self.model_class.id == id)
-        await session.execute(query)
+    async def delete(self, *, id: UUID, session: AsyncSession | None):
+        async for s in get_db_session():
+            session = s or session
+            query = delete(self.model_class).where(self.model_class.id == id)
+            await session.execute(query)

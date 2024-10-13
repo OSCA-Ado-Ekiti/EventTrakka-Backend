bcaf5ba0419f8848cca9b32a0eb80ce104d48677
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-10-13 18:36:38 +0100

    Update access token and refresh token expiry

--- app/core/config.py
+++ app/core/config.py
@@ -30,7 +30,8 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str
     # 60 minutes * 24 hours * 8 days = 8 days
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     FRONTEND_HOST: str = "http://localhost:5173"
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
 

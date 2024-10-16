3281d47bd454562c7bb5ace447800793f9e443bb
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-10-16 20:06:20 +0100

    Refactor attendee model

--- app/models/attendees.py
+++ app/models/attendees.py
@@ -1,16 +1,33 @@
-from datetime import datetime
-from sqlalchemy import Column, Integer, ForeignKey, String, Boolean, DateTime
+from uuid import UUID
+
+from pydantic import EmailStr
+from sqlmodel import Field
+
 from app.extras.models import BaseDBModel
 from app.managers import BaseModelManager
 
+
 class Attendee(BaseDBModel):
+    """Attendee is a representation of users that RSVP an `Event` (tech event) created by an
+    `Organization` (tech communities). Even though Attendees are real word users of the platform,
+    they're not `User` on Eventtrakka which is limited to admin and `Organization` members for
+    the sake of simplicity and may be changed in the future. Ateendees also do not need to be
+    signed in. They just surf Eventtrakka for `Event`s that interests them and RSVP the event
+    by filling up a from which results in the creation of this model.
+    """
+
     __tablename__ = "attendees"
 
-    event_id: int = Column(Integer, ForeignKey("events.id"), nullable=False)
-    user_id: int = Column(Integer, ForeignKey("users.id"), nullable=False)
-    status: str = Column(String(50), default="pending")  # RSVP status (status can be pending, confirmed, etc.)
-    checked_in: bool = Column(Boolean, default=False)
-    created_at: datetime = Column(DateTime, default=datetime.utcnow)
+    event_id: UUID = Field(
+        foreign_key="events",
+        ondelete="CASCADE",
+        description="The event the attendee RSVP",
+    )
+    attended_event: bool = Field(
+        False,
+        description="An event may be virtual or physical, this flag helps to indicate if the user attended the event",
+    )
+    email: EmailStr
 
     @property
     def objects(self):

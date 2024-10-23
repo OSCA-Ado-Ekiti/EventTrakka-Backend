190f66fc9c97d0ef2082f2c6397b578b81b3cde2
Gbenga Adeyi <adeyigbenga005@gmail.com>
2024-10-23 20:03:06 +0100

    Implement attendee model

--- app/models/attendees.py
+++ app/models/attendees.py
@@ -1,9 +1,43 @@
-from uuid import UUID
+from enum import Enum
+from typing import TYPE_CHECKING
+from uuid import UUID, uuid4
 
 from pydantic import EmailStr
-from sqlmodel import Field
+from sqlmodel import JSON, Field, Relationship, SQLModel
 
 from app.extras.models import BaseDBModel
+
+if TYPE_CHECKING:
+    from .events import Event
+
+
+class FieldType(str, Enum):
+    ATTENDEE_EMAIL = "ATTENDEE_EMAIL"
+    EMAIL = "EMAIL"
+    TEXT = "TEXT"
+    TEXTAREA = "TEXTAREA"
+    CHOICE = "CHOICE"
+
+
+class AttendeeQuestion(SQLModel):
+    id: UUID = Field(default_factory=uuid4)
+    is_required: bool = Field(True, description="is the field a required field")
+    label: str = Field(
+        description="what to show in the form as the question label. e.g. Email Address:, Event Expectation:"
+    )
+    field_type: FieldType = Field(
+        description=(
+            "this determines how the field should be rendered in the frontend,"
+            " for ATTENDEE_EMAIL, the value is match with the email field of the"
+            " attendee model and does not need to be rendered in the frontend as"
+            " email is always required weather an event requires a questionnaire or not"
+        )
+    )
+    choices: list[str] | None = Field(None)
+    allow_multiple_choices: bool = Field(
+        False,
+        description="when the field type is a CHOICE, should the submission allow",
+    )
 
 
 class Attendee(BaseDBModel):
@@ -18,12 +52,16 @@ class Attendee(BaseDBModel):
     __tablename__ = "attendees"
 
     event_id: UUID = Field(
-        foreign_key="events",
+        foreign_key="events.id",
         ondelete="CASCADE",
         description="The event the attendee RSVP",
     )
+    event: "Event" = Relationship()
     attended_event: bool = Field(
         False,
         description="An event may be virtual or physical, this flag helps to indicate if the user attended the event",
     )
-    email: EmailStr
+    email: EmailStr = Field(max_length=320)
+    questionnaire_submission: dict | None = Field(
+        sa_type=JSON(none_as_null=True),
+    )
